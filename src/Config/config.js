14ba71974caacc714856e559c1920d438d53213f
Khristian Rojas <khristianrojas15@gmail.com>
2021-03-25 00:23:46 -0300

    Add config physics

--- src/Config/config.js
+++ src/Config/config.js
@@ -4,5 +4,18 @@ export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
-  height: 600
+  height: 600,
+  zoom: 1,
+  pixelArt: true,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 0 },
+          debug: false // set to true to view zones
+      }
+  },
+  // scene: [
+  //     BootScene,
+  //     WorldScene
+  // ]
 };
