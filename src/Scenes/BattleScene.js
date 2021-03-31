29c45db2c319b4114ea475291feab078ca8cb402
Khristian Rojas <khristianrojas15@gmail.com>
2021-03-30 23:08:04 -0300

    Create units folder, enemy, unit and player files

--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -1,4 +1,7 @@
-/* eslint-disable no-undef, no-plusplus */
+/* eslint-disable no-undef, no-plusplus, max-classes-per-file */
+import PlayerCharacter from '../Units/Player';
+import Enemy from '../Units/Enemy';
+
 class BattleScene extends Phaser.Scene {
   constructor() {
     super('BattleScene');
@@ -112,67 +115,6 @@ class BattleScene extends Phaser.Scene {
       this.scene.switch('Game');
     }
     // return to WorldScene and sleep current BattleScene
-  }
-}
-
-// base class for heroes and enemies
-class Unit extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, texture, frame, type, hp, damage) {
-    super(scene, x, y, texture, frame);
-    this.type = type;
-    if (type === 'Warrior') {
-      this.maxHP = 100;
-    }
-    if (type === 'Mage') {
-      this.maxHP = 50;
-    }
-
-    this.hp = hp;
-    this.damage = damage; // default damage
-    // this.setScale(scale);
-
-    this.living = true;
-    this.menuItem = null;
-  }
-
-  // we will use this to notify the menu item when the unit is dead
-  setMenuItem(item) {
-    this.menuItem = item;
-  }
-
-  // attack the target unit
-  attack(target) {
-    if (target.living) {
-      target.takeDamage(this.damage);
-      this.scene.events.emit('Message', `${this.type} attacks ${target.type} for ${this.damage} damage`);
-    }
-  }
-
-  takeDamage(damage) {
-    this.hp -= damage;
-    if (this.hp <= 0) {
-      this.hp = 0;
-      this.menuItem.unitKilled();
-      this.living = false;
-      this.visible = false;
-      this.menuItem = null;
-    }
-  }
-}
-
-class Enemy extends Unit {
-  constructor(scene, x, y, texture, frame, type, hp, damage) {
-    super(scene, x, y, texture, frame, type, hp, damage, 0.8);
-    this.setScale(2);
-  }
-}
-
-class PlayerCharacter extends Unit {
-  constructor(scene, x, y, texture, frame, type, hp, damage) {
-    super(scene, x, y, texture, frame, type, hp, damage, 1.3);
-
-    this.flipX = true;
-    this.setScale(4);
   }
 }
 
---
+++ src/Units/Unit.js
@@ -0,0 +1,44 @@
+// base class for heroes and enemies
+export default class Unit extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, texture, frame, type, hp, damage) {
+    super(scene, x, y, texture, frame);
+    this.type = type;
+    if (type === 'Warrior') {
+      this.maxHP = 100;
+    }
+    if (type === 'Mage') {
+      this.maxHP = 50;
+    }
+
+    this.hp = hp;
+    this.damage = damage; // default damage
+    // this.setScale(scale);
+
+    this.living = true;
+    this.menuItem = null;
+  }
+
+  // we will use this to notify the menu item when the unit is dead
+  setMenuItem(item) {
+    this.menuItem = item;
+  }
+
+  // attack the target unit
+  attack(target) {
+    if (target.living) {
+      target.takeDamage(this.damage);
+      this.scene.events.emit('Message', `${this.type} attacks ${target.type} for ${this.damage} damage`);
+    }
+  }
+
+  takeDamage(damage) {
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      this.hp = 0;
+      this.menuItem.unitKilled();
+      this.living = false;
+      this.visible = false;
+      this.menuItem = null;
+    }
+  }
+}
