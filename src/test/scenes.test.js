212e2b482fec49acc707a34c3b7c85dbc3842c77
Khristian Rojas <khristianrojas15@gmail.com>
2021-04-01 10:42:31 -0300

    Add test for BootScene

--- src/test/scenes.test.js
+++ src/test/scenes.test.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import 'jest-canvas-mock';
 import Phaser from 'phaser';
 import BattleScene from '../Scenes/BattleScene';
+import BootScene from '../Scenes/BootScene';
 
 test('BattleScene is an instance of Scene', () => {
   expect(BattleScene.prototype instanceof Phaser.Scene).toBe(true);
@@ -9,4 +10,12 @@ test('BattleScene is an instance of Scene', () => {
 
 test('BattleScene is a function', () => {
   expect(typeof BattleScene).toBe('function');
+});
+
+test('BootScene is an instance of Scene', () => {
+  expect(BootScene.prototype instanceof Phaser.Scene).toBe(true);
+});
+
+test('BootScene is a function', () => {
+  expect(typeof BootScene).toBe('function');
 });
