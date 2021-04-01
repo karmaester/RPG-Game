e19aeb8d1ad4b16c3ac50d0e59db8e22bffbdb2c
Khristian Rojas <khristianrojas15@gmail.com>
2021-04-01 10:43:46 -0300

    Add test for CreditsScene

--- src/test/scenes.test.js
+++ src/test/scenes.test.js
@@ -3,6 +3,7 @@ import 'jest-canvas-mock';
 import Phaser from 'phaser';
 import BattleScene from '../Scenes/BattleScene';
 import BootScene from '../Scenes/BootScene';
+import CreditsScene from '../Scenes/CreditsScene';
 
 test('BattleScene is an instance of Scene', () => {
   expect(BattleScene.prototype instanceof Phaser.Scene).toBe(true);
@@ -18,4 +19,12 @@ test('BootScene is an instance of Scene', () => {
 
 test('BootScene is a function', () => {
   expect(typeof BootScene).toBe('function');
+});
+
+test('CreditsScene is an instance of Scene', () => {
+  expect(CreditsScene.prototype instanceof Phaser.Scene).toBe(true);
+});
+
+test('CreditsScene is a function', () => {
+  expect(typeof CreditsScene).toBe('function');
 });
